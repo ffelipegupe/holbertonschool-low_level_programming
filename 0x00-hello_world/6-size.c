42cd01baa69ce08d24168760fe6b4755e13f7f5c
ffelipegupe <1560@holbertonschool.com>
2020-02-06 17:57:00 +0000

    Add 6-size.c script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ long g;
 long long l;
 float f;
 
-printf("Sice of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Sice of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Sice of a long int: %lu byte(s)\n", (unsigned long)sizeof(g));
-printf("Sice of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
-printf("Sice of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(g));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
