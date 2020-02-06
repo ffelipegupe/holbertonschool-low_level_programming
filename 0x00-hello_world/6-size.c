214c128b148db611badfac54d2c53328423eba31
ffelipegupe <1560@holbertonschool.com>
2020-02-06 17:50:16 +0000

    Add 6-size.c script

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - run a function
+ *
+ * Description: prints the sixe of data types in bytes
+ *
+ * Return: 0
+ */
+int main(void)
+{
+  char c;
+  int i;
+  long g;
+  long long l;
+  float f;
+
+  printf("Sice of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+  printf("Sice of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
+  printf("Sice of a long int: %lu byte(s)\n", (unsigned long)sizeof(g));
+  printf("Sice of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+  printf("Sice of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+  return (0);
+}
