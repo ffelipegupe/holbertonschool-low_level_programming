983d623bfd8b42cc086d0b672abb9dd23dce77ee
ffelipegupe <1560@holbertonschool.com>
2020-02-06 18:08:09 +0000

    Add 101-quote.c script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - run the functions
+ *
+ * Description: include a string in the standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+write(2, "and that piece of art if useful - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
