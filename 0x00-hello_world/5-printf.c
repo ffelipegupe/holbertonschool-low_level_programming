d0d8f70a89626346625b661ca8e79a48086d0a0f
ffelipegupe <1560@holbertonschool.com>
2020-02-06 17:22:25 +0000

    Add 5-printf.c script

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <sdio.h>
+
+/**
+ * main - function that prints a string
+ *
+ * Description: prints a string using printf
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
+
