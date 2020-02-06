ed36fe05a67ab72f36eec9879b806dd7a9323657
ffelipegupe <1560@holbertonschool.com>
2020-02-06 16:45:30 +0000

    Add 4-puts.c script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<studio.h>
+
+/*
+ * main - function that prints a string
+ * 
+ * Description: prints a string using puts
+ *
+ * Return: 0 to end the program
+ *
+ */
+int main (void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
