fefa71a9eff6ab816730f1f5f93968b812670cb9
ffelipegupe <1560@holbertonschool.com>
2020-02-07 05:18:31 +0000

    Add 6-print_numberz script

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function that prints a string
+ *
+ * Description: function that prints the all single digits of base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
