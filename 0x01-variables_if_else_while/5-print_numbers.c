d0847866bd98e9fc3eee228e9e7fc6a0ac4a2546
ffelipegupe <1560@holbertonschool.com>
2020-02-07 04:56:09 +0000

    Add 5-print_numbers.c script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - prints numbers
+*
+* Description: function that prints the numbers from 0 to 10
+* Return: 0
+*/
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
