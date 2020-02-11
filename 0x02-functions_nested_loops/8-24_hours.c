a61806c06e7e4900f54cb417c06cd17e6cde9509
ffelipegupe <1560@holbertonschool.com>
2020-02-11 19:27:11 +0000

    Add 8-24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * jack_bauer - prints minutes
+ *
+ * Description: function that prints every minute from 00:00 to 23:59
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int n, m, p, q;
+
+	for (n = 0; n <= 2; n++)
+	{
+		for (m = 0; m <= 3; m++)
+		{
+			for (p = 0; p <= 5; p++)
+			{
+				for (q = 0; q <= 9; q++)
+				{
+					_putchar(n + '0');
+					_putchar(m + '0');
+					_putchar(':');
+					_putchar(p + '0');
+					_putchar(q + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
