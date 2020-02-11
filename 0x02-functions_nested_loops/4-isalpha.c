8cb0a576a6f248fa0ad41e8781e77a66df20e216
ffelipegupe <1560@holbertonschool.com>
2020-02-11 17:09:12 +0000

    Add 4-isalpha.c modified

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,9 @@
  */
 int _isalpha(int c)
 {
-	if(c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 		return (1);
-	else if(c <= 122 && c >= 97)
+	else if (c <= 122 && c >= 97)
 		return (1);
 	else
 		return (0);
