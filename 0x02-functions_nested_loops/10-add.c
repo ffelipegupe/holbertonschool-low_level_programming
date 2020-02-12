b6b0aa6066c7a8de47777542f739fa7b5cc2f036
ffelipegupe <1560@holbertonschool.com>
2020-02-12 04:47:32 +0000

    Add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * add - returns a number
+ * @n: number to be added
+ * @m: number to be added
+ *
+ * Description: function that adds two integers and returns the result
+ * Return: r, the value of the addition
+ */
+int add(int n, int m)
+{
+	int r;
+
+	r = n + m;
+
+	return (r);
+}
