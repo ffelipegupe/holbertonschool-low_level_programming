0a4f6243f883975fbb594ee33dbc4db81a0a2003
ffelipegupe <1560@holbertonschool.com>
2020-02-19 17:11:47 +0000

    Add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_rev - function that prints a string
+ * @s: string to be printed
+ *
+ * Description: function that prints a string in reverse
+ * Returns: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '0')
+	{
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
