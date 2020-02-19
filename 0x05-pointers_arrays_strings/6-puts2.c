e130e2dd02b5e55192713f40876857b81c456792
ffelipegupe <1560@holbertonschool.com>
2020-02-19 18:41:43 +0000

    Add 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * puts2 - function that prints a string
+ * @str: string to be checked
+ *
+ * Description: function that prints every other character of a string, startin
+ * g with the first character, followed by a new line
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{
+		_putchar(*(str + i));
+		i = i + 2;
+	}
+	_putchar('\n');
+}
