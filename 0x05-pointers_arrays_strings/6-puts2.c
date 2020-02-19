6f55c25307270dcaaed9d2b8cbc268948b9d855b
ffelipegupe <1560@holbertonschool.com>
2020-02-19 18:56:08 +0000

    Add 6-puts2.c modified

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,12 +11,12 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (*(str + i))
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
