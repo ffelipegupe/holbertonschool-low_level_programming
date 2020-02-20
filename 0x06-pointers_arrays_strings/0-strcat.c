9fbb35740fc03a8e1d35cf6c83b25a08d0b96a2d
ffelipegupe <1560@holbertonschool.com>
2020-02-20 14:37:23 +0000

    Add 0-strcar.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _strcat - function that prints strings
+ * @dest: string to be concatenated
+ * @src: string to be concatenated
+ *
+ * Description: function that concatenates two strings
+ * Returns: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	i++;
+	dest[i] = '\0';
+	return (dest);
+}
