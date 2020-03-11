759e58c94f2ad7bfcd6fccd40dfbd1b2b1527a72
ffelipegupe <1560@holbertonschool.com>
2020-03-11 17:12:11 +0000

    Add function_pointers.h updated

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
 #ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTER_H_
+#define _FUNCTION_POINTERS_H_
 
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
