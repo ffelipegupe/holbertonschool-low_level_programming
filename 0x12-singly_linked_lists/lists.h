cf35759fe5452e997ce723c085ce776f268383ec
ffelipegupe <1560@holbertonschool.com>
2020-03-20 03:53:04 +0000

    [Update] Updating lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,13 +2,6 @@
 #define LISTS_H_
 #include <stdlib.h>
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-int _putchar(char c);
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -24,5 +17,13 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+int _putchar(char c);
+
 
 #endif /* LISTS_H_ */
