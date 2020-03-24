ca6455dbdb92e6ba153c227c86265a39cc9e17ff
ffelipegupe <1560@holbertonschool.com>
2020-03-24 20:21:05 +0000

    [Edit] Editing 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+ #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
@@ -32,20 +32,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	while (tmp != NULL)
+	while (i != idx - 1)
 	{
-		if (i > idx)
-		{
-			return (NULL);
-		}
-		if (i == idx - 1)
-		{
-			new_node->n = n;
-			new_node->next = tmp->next;
-			tmp->next = new_node;
-		}
-		i++;
 		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+		i++;
 	}
+	new_node->n = n;
+	new_node->next = tmp->next;
+	tmp->next = new_node;
 	return (new_node);
 }
