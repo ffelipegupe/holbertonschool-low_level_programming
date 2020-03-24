5d7be9c734b417c11f2f7343b58706450b615f16
ffelipegupe <1560@holbertonschool.com>
2020-03-24 14:57:49 +0000

    [Edit] Editing 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	tmp = *head;
-	if(tmp == NULL)
+	if (tmp == NULL)
 		return (NULL);
 	if (idx == 0)
 	{
