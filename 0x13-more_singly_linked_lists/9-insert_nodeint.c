100724fe7c73399e614534d01bd911f903163aa3
ffelipegupe <1560@holbertonschool.com>
2020-03-24 14:31:05 +0000

    [Edit] Editing 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,12 +19,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	tmp = *head;
+	if(tmp == NULL)
+		return (NULL);
 	if (idx == 0)
 	{
 		new_node->n = n;
 		new_node->next = *head;
 		*head = new_node;
-		return (new_node);
+		return (*head);
 	}
 	while (tmp != NULL)
 	{
