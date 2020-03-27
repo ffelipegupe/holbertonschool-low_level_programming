1e4ed32291a94537138266698fc7ecc33a577b5e
ffelipegupe <1560@holbertonschool.com>
2020-03-27 00:07:49 +0000

    [Add] Adding 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: number to be represented
+ *
+ * Return: nothing
+ **/
+
+void print_binary(unsigned long int n)
+{
+	char c;
+	signed long int tam;
+	int ban;
+
+	tam = sizeof(n) * 8 - 1;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+	if (n == 1)
+	{
+		printf("1");
+		return;
+	}
+	ban = 0;
+	while (tam >= 0)
+	{
+		c = (n >> tam) & 1;
+		if(ban == 1)
+			putchar(c + '0');
+		else
+		{
+			if (c == 1)
+			{
+				putchar(c + '0');
+				ban = 1;
+			}
+		}
+		tam -= 1;
+	}
+}
