e9f87a217e08aa1f4cc2805415e90c3eaeba85ff
ffelipegupe <1560@holbertonschool.com>
2020-05-02 06:37:43 +0000

    [Edit] Editing function

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -20,11 +20,16 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = tmp->next;
-		free(tmp);
-		return (1);
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	       	return (1);
 	}
 	for (i = 0; tmp != NULL && i < index - 1; i++)
+	{
+		if (tmp == NULL)
+			return (-1);
 		tmp = tmp->next;
+	}
 	if (tmp == NULL || tmp->next == NULL)
 		return (-1);
 	next = tmp->next->next;
