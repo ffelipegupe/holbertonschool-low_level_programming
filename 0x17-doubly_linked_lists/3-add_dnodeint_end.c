6f3ddc9f6ab2446af760d5256d6f10a366694e8e
ffelipegupe <1560@holbertonschool.com>
2020-05-02 06:03:31 +0000

    [Edit] Editing function

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,7 +25,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	tmp = *head;
 	while (tmp->next != NULL)
-			tmp = tmp->next;
+		tmp = tmp->next;
 	tmp->next = new_node;
 	new_node->prev = tmp;
 	return (new_node);
