694008f39ea68ad9ca4cc8ae049220447a56fe5c
ffelipegupe <1560@holbertonschool.com>
2020-05-02 06:16:51 +0000

    [Edit] Editing function

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,10 +23,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h,n));
 	if (idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
 	while (i != idx - 1)
 	{
 		tmp = tmp->next;
