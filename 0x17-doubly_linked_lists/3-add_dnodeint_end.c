2aaeaa170e04b813a2e9d2645a965b282facd4d6
ffelipegupe <1560@holbertonschool.com>
2020-05-02 06:02:35 +0000

    [Edit] Editing function

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 	dlistint_t *tmp = *head;
 
-	new = malloc(sizeof(dlistint_t));
+	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
@@ -21,9 +21,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		new_node->prev = NULL;
 		*head = new_node;
-		return (new_node)
+		return (new_node);
 	}
-	last = *head;
+	tmp = *head;
 	while (tmp->next != NULL)
 			tmp = tmp->next;
 	tmp->next = new_node;
