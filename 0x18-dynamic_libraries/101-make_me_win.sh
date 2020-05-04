08c4f859717ce34f743018aa28c76684d6d92844
ffelipegupe <1560@holbertonschool.com>
2020-05-04 20:47:38 +0000

    Edit: editing 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ffelipegupe/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/libfoo.so
-export LD_PRELOAD=/tmp/libfoo.so
+wget -P /home/vagrant/holberton/holbertonschool-low_level_programming/0x18-dynamic_libraries  https://github.com/ffelipegupe/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/libfoo.so
+export LD_PRELOAD=/home/vagrant/holberton/holbertonschool-low_level_programming/0x18-dynamic_libraries/libfoo.so
