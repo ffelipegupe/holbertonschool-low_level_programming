f66448a2886a0435b83715db7cf42c49b4435a86
Felipe Gutierrez <1560@holbertonschool.com>
2020-05-29 18:46:00 -0500

    Edit: styling

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,7 +2,7 @@
 /**
  * hash_table_create - function that creates a hash table
  * @size: size of the array
- * 
+ *
  * Return: a pointer to the created table; NULL, on failure
  **/
 hash_table_t *hash_table_create(unsigned long int size)
@@ -10,10 +10,10 @@ hash_table_t *hash_table_create(unsigned long int size)
     hash_table_t *nht;
 
     if (size < 1)
-        return(NULL);
+        return (NULL);
 
     nht = malloc(sizeof(hash_table_t));
-    if(nht == NULL)
+    if (nht == NULL)
         return (NULL);
 
     nht->array = malloc(sizeof(hash_node_t) * size);
