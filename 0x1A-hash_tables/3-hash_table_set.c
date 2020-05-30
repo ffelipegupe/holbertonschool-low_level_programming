91ab972b47ec7afed5aad2b52fb7b164232e3e21
ffelipegupe <1560@holbertonschool.com>
2020-05-30 01:10:10 +0000

    Edit: loop

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,7 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	tmp = ht->array[idx];
 	if (tmp != NULL)
 	{
-		while (n)
+		while (tmp)
 		{
 			if (strcmp(key, tmp->key) == 0)
 			{
