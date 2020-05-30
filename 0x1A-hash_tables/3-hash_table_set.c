f7804f9e9ff94f9f1e67323c1f37df8cfc6ddec2
ffelipegupe <1560@holbertonschool.com>
2020-05-30 01:01:05 +0000

    Add: adding 3-hash_table.set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: hash table to update
+ * @key: the non-empty key
+ * @value: value associated to the key
+ *
+ * Return: 1, on succes; 0, otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node = NULL;
+	hash_node_t *tmp = NULL;
+	unsigned long int idx;
+
+	if (ht == NULL || key == NULL || strlen(key) <= 0)
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
+	while (tmp != NULL)
+	{
+		if (strcmp(key, tmp->key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
+	return (1);
+}
