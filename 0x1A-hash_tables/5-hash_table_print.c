470874f736b714e605afe15b5d01ff9abe46325f
ffelipegupe <1560@holbertonschool.com>
2020-05-30 01:47:55 +0000

    Edit: if condition

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,7 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int idx;
 	unsigned int i = 0;
 
-	if (ht = NULL)
+	if (ht == NULL)
 		return;
 	printf("{");
 	for (idx = 0; idx < ht->size; idx++)
