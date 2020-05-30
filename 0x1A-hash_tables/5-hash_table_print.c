75f363256cc9f1681574d1fde5b4c4cb56cc0e25
ffelipegupe <1560@holbertonschool.com>
2020-05-30 01:45:35 +0000

    Add: adding 5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: hash table to print
+ *
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *tmp = NULL;
+	unsigned long int idx;
+	unsigned int i = 0;
+
+	printf("{");
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		tmp = ht->array[idx];
+		while (tmp != NULL)
+		{
+			if (i == 1)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			i = 1;
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
+}
