3d5c47de94eace1acbecf37a81945d74d810c328
ffelipegupe <1560@holbertonschool.com>
2020-05-30 00:03:30 +0000

    Edit: styling

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	if (nht == NULL)
 		return (NULL);
-	nht->array = malloc(sizeof(hash_node_t) * size);
+	nht->array = malloc(sizeof(hash_node_t *) * size);
 	if (nht->array == NULL)
 		return (NULL);
 	nht->size = size;
