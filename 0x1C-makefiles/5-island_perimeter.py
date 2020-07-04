1de69cdf3092a0f036e37141667083800ab71a08
Felipe Gutierrez <1560@holbertonschool.com>
2020-07-03 23:27:28 -0500

    Add: adding 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Island Perimeter Module"""
+
+
+def island_perimeter(grid):
+    """Function to find an island perimeter based on grid"""
+    l = 0
+    for width in range(len(grid)):
+        for height in range(len(grid[1])):
+            if grid[width][height] == 1:
+                l += 4
+                if grid[width][height + 1] == 1:
+                    l -=1
+                if grid[width][height - 1] == 1:
+                    l -= 1
+                if grid[width + 1][height] == 1:
+                    l -= 1
+                if grid[width - 1][height] == 1:
+                    l -= 1    
+    return(l)
