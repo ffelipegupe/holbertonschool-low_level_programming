5fe1850827ea907a07fb28641f67fe98f54dfe10
ffelipegupe <1560@holbertonschool.com>
2020-08-27 19:34:19 +0000

    Add: adding file

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,12 +17,12 @@ int linear_search(int *array, size_t size, int value)
 {
 	unsigned int i = 0;
 
-	if (array == NULL)
+	if (!array)
 		return (-1);
 
 	while (i < size)
 	{
-		printf("Value checked array [%u] = [%d]\n", i, array[i]);
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 		i++;
